e0d76d2748709bb7bae1447a1c09360713c2dd39
Szőke Barnabás <szokebarnabas85@gmail.com>
2015-08-02 10:44:59 +0100

    Gradle wrapper has been changed

--- support-eureka/src/main/java/org/landlord/support/eureka/Application.java
+++ support-eureka/src/main/java/org/landlord/support/eureka/Application.java
@@ -2,9 +2,10 @@ package org.landlord.support.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
